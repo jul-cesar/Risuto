2e1f8ddffb4cbdd7b23f3e474d46d0d2d89f9152
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-05-14 20:26:04 -0500

    feat: lists related actions

---
+++ src/actions/lists-actions.ts
@@ -0,0 +1,163 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { db } from "../db";
+import { List, ListBooks, Lists, NewList } from "../db/schema";
+
+interface response<T> {
+  success: boolean;
+  message: string;
+  data?: T;
+}
+
+export const createList = async (list: NewList): Promise<response<List>> => {
+  try {
+    if (!list || Object.keys(list).length === 0) {
+      return {
+        success: false,
+        message: "List data is required and cannot be empty",
+      };
+    }
+
+    const newList = await db.insert(Lists).values(list).returning().get();
+    if (!newList) {
+      return {
+        success: false,
+        message: "Failed to create list",
+      };
+    }
+    return {
+      success: true,
+      message: "List created successfully",
+      data: newList,
+    };
+  } catch (error: unknown) {
+    console.error(
+      "Error creating list:",
+      error instanceof Error ? error.message : error
+    );
+    return {
+      success: false,
+      message: "An unexpected error occurred while creating the list",
+    };
+  }
+};
+
+export const editList = async (
+  listId: string,
+  updatedList: Partial<NewList>
+): Promise<response<List>> => {
+  try {
+    if (!listId || !updatedList || Object.keys(updatedList).length === 0) {
+      return {
+        success: false,
+        message: "List ID and updated data are required",
+      };
+    }
+
+    const updated = await db
+      .update(Lists)
+      .set(updatedList)
+      .where(eq(Lists.id, listId))
+      .returning()
+      .get();
+
+    if (!updated) {
+      return {
+        success: false,
+        message: "Failed to update list",
+      };
+    }
+    return {
+      success: true,
+      message: "List updated successfully",
+      data: updated,
+    };
+  } catch (error: unknown) {
+    console.error(
+      "Error updating list:",
+      error instanceof Error ? error.message : error
+    );
+    return {
+      success: false,
+      message: "An unexpected error occurred while updating the list",
+    };
+  }
+};
+
+export const deleteList = async (listId: string): Promise<response<void>> => {
+  try {
+    if (!listId) {
+      return {
+        success: false,
+        message: "List ID is required",
+      };
+    }
+
+    const deleted = await db
+      .delete(Lists)
+      .where(eq(Lists.id, listId))
+      .returning()
+      .get();
+
+    if (!deleted) {
+      return {
+        success: false,
+        message: "Failed to delete list",
+      };
+    }
+    return {
+      success: true,
+      message: "List deleted successfully",
+    };
+  } catch (error: unknown) {
+    console.error(
+      "Error deleting list:",
+      error instanceof Error ? error.message : error
+    );
+    return {
+      success: false,
+      message: "An unexpected error occurred while deleting the list",
+    };
+  }
+};
+
+export const addBookToList = async (
+  listId: string,
+  bookId: string
+): Promise<response<void>> => {
+  try {
+    if (!listId || !bookId) {
+      return {
+        success: false,
+        message: "List ID and Book ID are required",
+      };
+    }
+
+    const result = await db
+      .insert(ListBooks)
+      .values({ book_id: bookId, list_id: listId })
+      .returning()
+      .get();
+
+    if (!result) {
+      return {
+        success: false,
+        message: "Failed to add book to list",
+      };
+    }
+    return {
+      success: true,
+      message: "Book added to list successfully",
+    };
+  } catch (error: unknown) {
+    console.error(
+      "Error adding book to list:",
+      error instanceof Error ? error.message : error
+    );
+    return {
+      success: false,
+      message: "An unexpected error occurred while adding the book to the list",
+    };
+  }
+};
