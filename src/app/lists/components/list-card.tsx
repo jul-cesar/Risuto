a53c02a7231a779796d8d52ee6ff4786a64f73cb
Laaguna <73413231+Laaguna@users.noreply.github.com>
2025-05-19 21:20:08 -0500

    feat: get liked lists

---
+++ src/app/lists/components/list-card.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Book } from "@/db/schema";
+
+export const ListCard = ({ list }: any) => {
+  return (
+    <Link key={list.id} href={`/lists/${list.slug}`}>
+              <Card className="transition hover:scale-[1.02] hover:shadow-xl">
+                <CardHeader>
+                  <CardTitle className="flex items-center gap-2 text-lg">
+                    <span className="inline-block w-2 h-2 bg-primary rounded-full" />
+                    {list.title}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4">
+                  <div className="grid grid-cols-2 gap-2">
+                    {list.books.map((book: Book) => (
+                      <div
+                        key={book.id}
+                        className="aspect-square bg-muted rounded-lg overflow-hidden"
+                      >
+                        <Image
+                          src={book.cover_url}
+                          alt={book.title}
+                          width={200}
+                          height={200}
+                          className="object-cover"
+                        />
+                      </div>
+                    ))}
+
+                    {list.books.length < 6 &&
+                      Array.from({ length: 6 - list.books.length }).map((_, i) => (
+                        <div
+                          key={`ph-${i}`}
+                          className="aspect-square bg-muted rounded-lg animate-pulse"
+                        />
+                      ))}
+                  </div>
+
+                  {list.description && (
+                    <p className="text-sm text-muted-foreground line-clamp-2">
+                      {list.description}
+                    </p>
+                  )}
+                </CardContent>
+
+                <CardFooter className="flex items-center justify-between">
+                  {/* Avatar del creador */}
+                  <div className="flex items-center gap-2">
+                    <Avatar>
+                      <AvatarImage src={list.user.profileImageUrl} />
+                      <AvatarFallback>
+                        {list.user.fullName.charAt(0)}
+                      </AvatarFallback>
+                    </Avatar>
+                    <span className="text-sm">{list.user.username}</span>
+                  </div>
+
+                  <span className="text-xs text-muted-foreground">
+                    {new Date(list.updatedAt).toLocaleDateString()}
+                  </span>
+                </CardFooter>
+              </Card>
+            </Link>
+  );
+}
--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-import { Card, CardHeader, CardTitle, CardContent, CardFooter } from "@/components/ui/card";
-import { getAllLists } from "@/actions/lists-actions";
-import Link from "next/link";
 import { getUserClerk } from "@/actions/clerk-actions";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getAllLists } from "@/actions/lists-actions";
+import { ListCard } from "./components/list-card";
+import { Tabs } from "@radix-ui/react-tabs";
+import { TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { getListsLikedByUser } from "@/actions/likes-actions";
+import { currentUser } from "@clerk/nextjs/server";
 
 type ListDataItem = NonNullable<Awaited<ReturnType<typeof getAllLists>>["data"]>[0];
 
@@ -17,11 +18,13 @@ interface ListWithBooksAndUser extends ListDataItem {
 }
 
 export default async function ListsPage() {
-  const response = await getAllLists();
-  const lists = response.data ?? [];
+  
+  const user = await currentUser();
+  const responseAllLists = await getAllLists();
+  const allLists = responseAllLists.data ?? [];
   
   const listsWithUser: ListWithBooksAndUser[] = await Promise.all(
-    lists.map(async (list) => {
+    allLists.map(async (list) => {
       const u = await getUserClerk(list.user_id);
       return {
         ...list,
@@ -35,6 +38,23 @@ export default async function ListsPage() {
     })
   ); 
 
+  const responselikedLists = await getListsLikedByUser(user?.id ?? "");
+  const likedLists = responselikedLists.data ?? [];
+   const listsLikedWithUser: ListWithBooksAndUser[] = await Promise.all(
+    likedLists.map(async (list) => {
+      const u = await getUserClerk(list.user_id);
+      return {
+        ...list,
+        user: {
+          id: u.id,
+          fullName: u.fullName ?? u.username ?? "Unknown",
+          username: u.username ?? "unknown",
+          profileImageUrl: u.imageUrl ?? "",
+        },
+      };
+    })
+  );
+
   return (
     <main className="flex-1 bg-gradient-to-b from-background-secondary to-background text-foreground font-mono">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -43,70 +63,32 @@ export default async function ListsPage() {
           <h1 className="text-2xl">🔥 Last added lists</h1>
         </header>
 
-        {/* Lists */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {listsWithUser.map((list) => (
-            <Link key={list.id} href={`/lists/${list.slug}`}>
-              <Card className="transition hover:scale-[1.02] hover:shadow-xl">
-                <CardHeader>
-                  <CardTitle className="flex items-center gap-2 text-lg">
-                    <span className="inline-block w-2 h-2 bg-primary rounded-full" />
-                    {list.title}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className="flex flex-col gap-4">
-                  <div className="grid grid-cols-2 gap-2">
-                    {list.books.map((book) => (
-                      <div
-                        key={book.id}
-                        className="aspect-square bg-muted rounded-lg overflow-hidden"
-                      >
-                        <Image
-                          src={book.cover_url}
-                          alt={book.title}
-                          width={200}
-                          height={200}
-                          className="object-cover"
-                        />
-                      </div>
-                    ))}
+        <Tabs defaultValue="All public lists">
+          <TabsList className ="grid w-full grid-cols-3 mb-12">
+            <TabsTrigger value ="All public lists">All public lists</TabsTrigger>
+            <TabsTrigger value ="My lists">My lists</TabsTrigger>
+            <TabsTrigger value ="Liked lists">Liked lists</TabsTrigger>
+          </TabsList>
+          <TabsContent value="All public lists">
+            {/* Lists */}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {listsWithUser.map((list) => (
+                <ListCard key={list.id} list={list} />
+              ))}
+            </div>
+          </TabsContent>
+          <TabsContent value="My lists">
+            
 
-                    {list.books.length < 6 &&
-                      Array.from({ length: 6 - list.books.length }).map((_, i) => (
-                        <div
-                          key={`ph-${i}`}
-                          className="aspect-square bg-muted rounded-lg animate-pulse"
-                        />
-                      ))}
-                  </div>
-
-                  {list.description && (
-                    <p className="text-sm text-muted-foreground line-clamp-2">
-                      {list.description}
-                    </p>
-                  )}
-                </CardContent>
-
-                <CardFooter className="flex items-center justify-between">
-                  {/* Avatar del creador */}
-                  <div className="flex items-center gap-2">
-                    <Avatar>
-                      <AvatarImage src={list.user.profileImageUrl} />
-                      <AvatarFallback>
-                        {list.user.fullName.charAt(0)}
-                      </AvatarFallback>
-                    </Avatar>
-                    <span className="text-sm">{list.user.username}</span>
-                  </div>
-
-                  <span className="text-xs text-muted-foreground">
-                    {new Date(list.updatedAt).toLocaleDateString()}
-                  </span>
-                </CardFooter>
-              </Card>
-            </Link>
-          ))}
-        </div>
+          </TabsContent>
+          <TabsContent value="Liked lists">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {listsLikedWithUser.map((list) => (
+                <ListCard key={list.id} list={list} />
+              ))}
+            </div>
+          </TabsContent>
+        </Tabs>
       </div>
     </main>
   );
